d001fb6935562c11a99dd61964281ab141962519
inn-jeong <jinpal429@gmail.com>
2023-05-31 18:24:13 +0900

    230531 arcademy add

--- spring_15/src/main/java/com/lgy/spring15/domain/SampleVO.java
+++ spring_15/src/main/java/com/lgy/spring15/domain/SampleVO.java
@@ -6,10 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-//@Getter
-//@Setter
-//@AllArgsConstructor
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
 public class SampleVO {
 	private int mno;
 	private String firstName;
