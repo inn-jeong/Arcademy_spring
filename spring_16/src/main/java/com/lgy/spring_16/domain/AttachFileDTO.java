56b86b6205fe4e12a838022727407336c7b59065
inn-jeong <jinpal429@gmail.com>
2023-06-05 19:09:13 +0900

    230605 arcademy add

--- spring_16/src/main/java/com/lgy/spring_16/domain/AttachFileDTO.java
+++ spring_16/src/main/java/com/lgy/spring_16/domain/AttachFileDTO.java
@@ -5,5 +5,8 @@ import lombok.Data;
 @Data
 public class AttachFileDTO {
 	private String fileName;
-	private String filePath;
+//	private String filePath;
+	private String uploadPath;
+	private String uuid;
+	private boolean image;
 }
