1e6f239865e62ca13877e1bdff533cd01947dd19
inn-jeong <jinpal429@gmail.com>
2023-06-22 19:37:52 +0900

    230622 arcademy

---
+++ spring_tran/src/main/java/com/lgy/spring_tran/dto/TicketDto.java
@@ -0,0 +1,12 @@
+package com.lgy.spring_tran.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TicketDto {
+	private String consumerId;
+//	private int countnum;
+	private int amount;
+}
